18acd93b54768799bed92f246cbcf09f02e8c1fd
Anil Kumar Chillara <anil.chillara@outlook.com>
2022-07-16 22:58:28 -0700

    Initial project setup for 75 must do Leet Code problem.

---
+++ MustDo75/src/test/java/com/anil/MustDo75/MustDo75ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anil.MustDo75;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustDo75ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
