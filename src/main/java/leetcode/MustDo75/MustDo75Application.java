a2b40b80b1d7a78e595d69bff320436b76af8ab0
Anil Kumar Chillara <anil.chillara@outlook.com>
2022-07-17 00:23:19 -0700

    Refactor

---
+++ src/main/java/leetcode/MustDo75/MustDo75Application.java
@@ -0,0 +1,13 @@
+package leetcode.MustDo75;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustDo75Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustDo75Application.class, args);
+	}
+
+}
